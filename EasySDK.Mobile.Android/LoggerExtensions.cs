1e273c61fa6de5d7c1a6d017e94c5f6460a38bdf
olinyavod <olinyavod@gmail.com>
2022-03-13 22:07:52 +1000

    Add logger supports

---
+++ EasySDK.Mobile.Android/LoggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using EasySDK.Mobile.ViewModels.Services;
+using NLog;
+using NLog.Targets;
+
+namespace EasySDK.Mobile.Android;
+
+public static class LoggerExtensions
+{
+	public static void DefaultNLogsConfigure(this IPathsService pathsService)
+	{
+		var config = new NLog.Config.LoggingConfiguration();
+		var logFile = new FileTarget("logfile")
+		{
+			FileName = pathsService.GetLogsFilePath(),
+			Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}|${exception:format=tostring}",
+			MaxArchiveFiles = 3,
+			ArchiveFileKind = FilePathKind.Relative,
+			ArchiveNumbering = ArchiveNumberingMode.DateAndSequence,
+			ArchiveEvery = FileArchivePeriod.Day
+		};
+		var logConsole = new ConsoleTarget("logconsole")
+		{
+			Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}|${exception:format=tostring}",
+		};
+
+		config.AddRule(LogLevel.Info, LogLevel.Fatal, logConsole);
+		config.AddRule(LogLevel.Debug, LogLevel.Fatal, logFile);
+
+		LogManager.Configuration = config;
+	}
+}
---
+++ EasySDK.Mobile.Android/Services/AndroidPathsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using EasySDK.Mobile.ViewModels.Services;
+
+namespace EasySDK.Mobile.Android.Services;
+
+public class DefaultAndroidPathsService : IPathsService
+{
+	public string GetLogsFilePath()
+	{
+		var personalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+		return Path.Combine(personalFolder, "Logs", "logs.log");
+	}
+}
