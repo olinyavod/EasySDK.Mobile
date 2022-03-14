50e54d34e655b35ebf028c458b69be5adfe31e4b
olinyavod <olinyavod@gmail.com>
2022-03-14 12:14:12 +1000

    Add Fake Data provider

---
+++ EasySDK.Mobile.FakeProviders/FakeCrudServiceBase.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using EasySDK.Mobile.FakeProviders.Models;
+using EasySDK.Mobile.Models;
+
+namespace EasySDK.Mobile.FakeProviders;
+
+public abstract class FakeCrudServiceBase<TModel, TKey>
+{
+	#region Private fields
+
+	private readonly Func<TModel, TKey> _getKey;
+	private readonly int _delayMilliseconds;
+
+	#endregion
+
+	#region Properties
+
+	protected IFixture Fixture { get; }
+	protected Dictionary<TKey, TModel> Items { get; }
+
+	#endregion
+
+	#region ctor
+
+	protected FakeCrudServiceBase(int count, Func<TModel, TKey> getKey, int delayMilliseconds = 500)
+	{
+		_getKey = getKey;
+		_delayMilliseconds = delayMilliseconds;
+		Fixture = new Fixture();
+
+		Items = GenerateItems(Fixture, count).ToDictionary(i => getKey(i), i => i);
+	}
+
+	#endregion
+
+	#region Public methods
+
+	public Task<IResponse<IEnumerable<TModel>>> GetList(IListRequest request) => Task.Run(async () =>
+	{
+		await Delay();
+		return FakeResponse.FromResult(Items
+			.Values
+			.Where(i => Search(i, request.Search))
+			.Skip(request.Offset)
+			.Take(request.Count));
+	});
+
+	public Task<IResponse<TModel>> GetByKey(TKey key) => Task.Run(async () =>
+	{
+		await Delay();
+
+		return Items.TryGetValue(key, out var value)
+			? FakeResponse.FromResult(value)
+			: FakeResponse.FromErrorCode<TModel>(ResponseErrorCodes.NotFound);
+	});
+
+	public Task<IResponse<bool>> Edit(TModel model) => Task.Run(async () =>
+	{
+		await Delay();
+		var key = _getKey(model);
+
+		if (!Items.ContainsKey(key))
+			return FakeResponse.FromErrorCode<bool>(ResponseErrorCodes.NotFound);
+
+		Items[key] = model;
+		return FakeResponse.FromResult(true);
+	});
+
+	public Task<IResponse<bool>> Delete(TKey key) => Task.Run(async () =>
+	{
+		await Delay();
+
+		return Items.Remove(key)
+			? FakeResponse.FromResult(true)
+			: FakeResponse.FromErrorCode<bool>(ResponseErrorCodes.NotFound);
+	});
+
+	#endregion
+
+	#region Protected methods
+
+	protected abstract bool Search(TModel model, string query);
+
+	protected async Task Delay()
+	{
+		if(_delayMilliseconds == 0)
+			return;
+
+		await Task.Delay(_delayMilliseconds);
+	}
+
+	protected IEnumerable<TModel> GenerateItems(IFixture fixture, int count)
+	{
+		Configure(fixture);
+
+		return fixture.CreateMany<TModel>(count);
+	}
+
+	protected abstract void Configure(IFixture fixture);
+
+	#endregion
+}
+
---
+++ EasySDK.Mobile.FakeProviders/Models/FakeResponse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using EasySDK.Mobile.Models;
+
+namespace EasySDK.Mobile.FakeProviders.Models;
+
+class FakeResponse : IResponse
+{
+	#region Properties
+
+	public bool HasError => ErrorCode > 0;
+
+	public int ErrorCode { get; set; }
+
+	public string ErrorMessage { get; set; }
+
+	public Dictionary<string, IEnumerable<string>> ErrorMessages { get; } = new();
+
+	public bool NeedAuthorization => ErrorCode == ResponseErrorCodes.Unauthorized;
+
+	#endregion
+
+	public static IResponse<TResult> FromResult<TResult>(TResult result) => new FakeResponse<TResult>(result);
+
+	public static IResponse<TResult> FromErrorCode<TResult>(int errorCode) => new FakeResponse<TResult>
+	{
+		ErrorCode = errorCode
+	};
+}
+
+class FakeResponse<TResult> : FakeResponse, IResponse<TResult>
+{
+	#region Properties
+
+	public TResult Result { get; }
+
+	#endregion
+
+	#region ctor
+
+	public FakeResponse()
+	{
+		
+	}
+
+	public FakeResponse(TResult result)
+	{
+		Result = result;
+	}
+
+	public IResponse<TNewResult> Convert<TNewResult>(Func<TResult, TNewResult> convert) => new FakeResponse<TNewResult>(convert(Result));
+
+	#endregion
+}
