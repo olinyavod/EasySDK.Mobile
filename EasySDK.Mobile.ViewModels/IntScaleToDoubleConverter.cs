24ffb948427de4ef4003d8a2feed0fafcec9cdba
olinyavod <olinyavod@gmail.com>
2022-12-30 22:21:53 +1000

    Price list page was added

---
+++ EasySDK.Mobile.ViewModels/IntScaleToDoubleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EasySDK.Mobile.ViewModels;
+
+public class IntScaleToDoubleConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is int i && parameter is double d)
+			return i * d;
+
+		return 0.0;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
