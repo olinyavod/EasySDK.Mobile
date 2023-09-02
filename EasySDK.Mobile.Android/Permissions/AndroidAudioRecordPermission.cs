d763bfcc94d730babf6ce90a1c12c7c939340228
olinyavod <olinyavod@gmail.com>
2023-09-02 16:33:39 +1000

    SIP call was fixed

--- EasySDK.Mobile.Android/Permissions/AndroidAudioRecordPermission.cs
+++ EasySDK.Mobile.Android/Permissions/AndroidAudioRecordPermission.cs
@@ -1,20 +1,33 @@
 ﻿#nullable enable
 
 using Android;
+using Android.OS;
 using EasySDK.Mobile.Android.Permissions;
 using EasySDK.Mobile.ViewModels.Permissions;
 using Xamarin.Forms;
 
 [assembly:Dependency(typeof(AndroidAudioRecordPermission))]
+
 namespace EasySDK.Mobile.Android.Permissions
 {
-	public class AndroidAudioRecordPermission : Xamarin.Essentials.Permissions.BasePlatformPermission, IAudioRecordPermission
+	public class AndroidAudioRecordPermission : Xamarin.Essentials.Permissions.BasePlatformPermission,
+		IAudioRecordPermission
 	{
-		public override (string androidPermission, bool isRuntime)[] RequiredPermissions { get; } =
+		public override (string androidPermission, bool isRuntime)[] RequiredPermissions
 		{
-			(Manifest.Permission.RecordAudio, true),
-			//(Manifest.Permission.ReadContacts, true),
-			//(Manifest.Permission.CaptureAudioOutput, true),
-		};
+			get
+			{
+				if (Build.VERSION.SdkInt > BuildVersionCodes.S)
+					return new[]
+					{
+						(Manifest.Permission.RecordAudio, true),
+						(Manifest.Permission.BluetoothConnect, true),
+						("android.permission.NEARBY_WIFI_DEVICES", true)
+					};
+
+				return new[] {(Manifest.Permission.RecordAudio, true)};
+
+			}
+		}
 	}
 }
