9ce13c798cb500c5cf268c48dcd101e2e4340f84
olinyavod <olinyavod@gmail.com>
2024-04-24 23:17:27 +1000

    Maui library was added

---
+++ EasySDK.Mobile.Maui/Services/MauiClipboardService.cs
@@ -0,0 +1,8 @@
+﻿using EasySDK.Mobile.ViewModels.Services;
+
+namespace EasySDK.Mobile.Maui.Services;
+
+public class MauiClipboardService : IClipboardService
+{
+	public Task SetTextAsync(string text) => Clipboard.SetTextAsync(text);
+}
---
+++ EasySDK.Mobile.Maui/Services/MauiShareService.cs
@@ -0,0 +1,11 @@
+﻿using EasySDK.Mobile.ViewModels.Services;
+
+namespace EasySDK.Mobile.Maui.Services;
+
+public class MauiShareService : IShareService
+{
+	public Task ShareFileAsync(string filePath, string mimeType)
+	{
+		return Share.RequestAsync(new ShareFileRequest(Path.GetFileName(filePath), new ShareFile(filePath, mimeType)));
+	}
+}
