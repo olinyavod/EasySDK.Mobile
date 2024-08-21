df4b5300333862fd9da9bc7e17f7212fde5d052b
olinyavod <olinyavod@gmail.com>
2024-08-21 16:50:05 +1000

    Load orders from provider was added

---
+++ EasySDK.Mobile.ViewModels/Services/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EasySDK.Mobile.Models;
+
+namespace EasySDK.Mobile.ViewModels.Services;
+
+public interface IDataService<TModel, TItem, TKey>
+{
+	Task<IResponseList<TItem>> GetListAsync(IListRequest request, CancellationToken token);
+
+	Task<IResponse<TModel?>> GetByIdAsync(TKey id, CancellationToken cancellationToken);
+}
