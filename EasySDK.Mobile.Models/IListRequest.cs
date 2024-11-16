ee433d15f6e59822eadb384ac0a311ba55365ba0
olinyavod <olinyavod@gmail.com>
2024-11-16 19:15:05 +1000

    Load voting list was added

--- EasySDK.Mobile.Models/IListRequest.cs
+++ EasySDK.Mobile.Models/IListRequest.cs
@@ -2,6 +2,15 @@
 {
 	public interface IListRequest
 	{
+		class EmptyRequest : IListRequest
+		{
+			public int Offset { get; set; }
+			public int Count { get; set; }
+			public string? Search { get; set; }
+		}
+
+		public static IListRequest Empty { get; } = new EmptyRequest();
+
 		#region Properties
 
 		int Offset { get; set; }
