df3ca54deb019df56b3ece492d90a083951d3cbb
olinyavod <olinyavod@gmail.com>
2024-12-21 15:22:37 +1000

    Migrate To NET9

--- EasySDK.Mobile.Maui/Services/ShellViewModelNavigationService.cs
+++ EasySDK.Mobile.Maui/Services/ShellViewModelNavigationService.cs
@@ -1,62 +1,12 @@
-﻿using EasySDK.Mobile.ViewModels.Extensions;
-using EasySDK.Mobile.ViewModels.Services;
+﻿namespace EasySDK.Mobile.Maui.Services;
 
-namespace EasySDK.Mobile.Maui.Services;
-
-public class ShellViewModelNavigationService : IViewModelNavigationService
+public class ShellViewModelNavigationService : ShellViewModelNavigationServiceBase
 {
-	#region Public methods
-
-	public Task GoToAsync<TViewModel>(object? parameter = null, bool animate = true, bool removeCurrent = false) where TViewModel : class
-	{
-		var viewName = typeof(TViewModel).GetViewKey();
-
-		return GoToAsync(viewName, parameter, animate, removeCurrent);
-	}
-
-	public Task GoToRootAsync<TViewModel>(object? parameter = null, bool animate = true, bool removeCurrent = false) where TViewModel : class
-	{
-		var viewName = typeof(TViewModel).GetViewKey();
-
-		return GoToAsync($"//{viewName}", parameter, animate, removeCurrent);
-	}
-
-	public Task GoToBackAsync(bool animate = true) => Current.GoToAsync("..", animate);
-
-	#endregion
-
 	#region Private methods
 
-	private Shell Current => Shell.Current;
+	protected override Shell GetShell() => Shell.Current;
 
-	private async Task GoToAsync(string route, object? parameter, bool animate, bool removeCurrent)
-	{
-		var currentShell = Current;
-		var currentPage = currentShell.CurrentPage;
-		var isModal = currentShell.Navigation.ModalStack.Contains(currentPage);
-
-		try
-		{
-			if (isModal)
-				_ = currentShell.Navigation.PopModalAsync(false);
-
-			if (parameter == null)
-			{
-				await Shell.Current.GoToAsync(route, animate);
-				return;
-			}
-			
-			await Current.GoToAsync(route, animate, new Dictionary<string, object>
-			{
-				{"Parameter", parameter}
-			});
-		}
-		finally
-		{
-			if (removeCurrent && !isModal)
-				currentShell.Navigation.RemovePage(currentPage);
-		}
-	}
+	
 
 	#endregion
 }
---
+++ EasySDK.Mobile.Maui/Services/ShellViewModelNavigationServiceBase.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using EasySDK.Mobile.ViewModels.Extensions;
+using EasySDK.Mobile.ViewModels.Services;
+
+namespace EasySDK.Mobile.Maui.Services;
+
+public abstract class ShellViewModelNavigationServiceBase : IViewModelNavigationService
+{
+	#region Public methods
+
+	public Task GoToAsync<TViewModel>(object? parameter = null, bool animate = true, bool removeCurrent = false) where TViewModel : class
+	{
+		var viewName = typeof(TViewModel).GetViewKey();
+
+		return GoToAsync(viewName, parameter, animate, removeCurrent);
+	}
+
+	public Task GoToRootAsync<TViewModel>(object? parameter = null, bool animate = true, bool removeCurrent = false) where TViewModel : class
+	{
+		var viewName = typeof(TViewModel).GetViewKey();
+
+		return GoToAsync($"//{viewName}", parameter, animate, removeCurrent);
+	}
+
+	public Task GoToBackAsync(bool animate = true) => GetShell().GoToAsync("..", animate);
+
+	#endregion
+
+	#region Protected methods
+
+	protected abstract Shell GetShell();
+
+	#endregion
+
+	#region Private methods
+
+	private async Task GoToAsync(string route, object? parameter, bool animate, bool removeCurrent)
+	{
+		var currentShell = GetShell();
+		var currentPage = currentShell.CurrentPage;
+		var isModal = currentShell.Navigation.ModalStack.Contains(currentPage);
+
+		try
+		{
+			if (isModal)
+				_ = currentShell.Navigation.PopModalAsync(false);
+
+			if (parameter == null)
+			{
+				await currentShell.GoToAsync(route, animate);
+				return;
+			}
+
+			await currentShell.GoToAsync(route, animate, new Dictionary<string, object>
+			{
+				{"Parameter", parameter}
+			});
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine("[{0}] Navigation error: {1}.", this, ex);
+			throw;
+		}
+		finally
+		{
+			if (removeCurrent && !isModal)
+				currentShell.Navigation.RemovePage(currentPage);
+		}
+	}
+
+	#endregion
+}
