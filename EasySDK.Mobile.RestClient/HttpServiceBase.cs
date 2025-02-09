398884efccee9f2afac6a46fa101a12029310ece
olinyavod <olinyavod@gmail.com>
2025-02-09 20:19:18 +1000

    Open push after destroy app was fixed

--- EasySDK.Mobile.RestClient/HttpServiceBase.cs
+++ EasySDK.Mobile.RestClient/HttpServiceBase.cs
@@ -328,7 +328,7 @@ public abstract class HttpServiceBase
 			}
 
 			var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-			Logger.LogDebug("Response content: {0}", content);
+				Logger.LogDebug("Response content: {0}", content);
 
 			stopwatch.Stop();
 			Logger.LogInformation
