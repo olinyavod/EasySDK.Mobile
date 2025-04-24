ea8241695386909b30ba4b706d381f3c9bc64bcc
olinyavod <olinyavod@gmail.com>
2025-04-24 23:24:26 +1000

    Load annoncment was added

---
+++ EasySDK.Mobile.Maui/Converters/HtmlEntityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using EasySDK.Mobile.Maui.Extensions;
+
+namespace EasySDK.Mobile.Maui.Converters;
+
+public class HtmlEntityConverter : IValueConverter, IMarkupExtension
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is string strValue)
+			return HtmlEntityProcessor.DecodeHtmlEntities(strValue);
+		return value;
+	}
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+
+	public object ProvideValue(IServiceProvider serviceProvider)
+	{
+		return this;
+	}
+}
---
+++ EasySDK.Mobile.Maui/Extensions/HtmlEntityProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EasySDK.Mobile.Maui.Extensions;
+
+public static partial class HtmlEntityProcessor
+{
+	[GeneratedRegex(@"&(?:#(\d+)|#x([\da-fA-F]+)|([a-zA-Z][\w\d]*));", RegexOptions.Compiled|RegexOptions.IgnoreCase|RegexOptions.CultureInvariant)]
+	private static partial Regex HtmlEntityRegex();
+
+	public static string DecodeHtmlEntities(string input)
+	{
+		if (string.IsNullOrWhiteSpace(input))
+			return input;
+        
+		var regex = HtmlEntityRegex();
+
+		return regex.Replace(input, match =>
+		{
+			var numericValue = match.Groups[1].Success ? int.Parse(match.Groups[1].Value) : (
+				match.Groups[2].Success ? Convert.ToInt32(match.Groups[2].Value, 16) : -1);
+            
+			if (numericValue != -1)
+				return char.ConvertFromUtf32(numericValue);
+
+			return match.Groups[3].Value switch
+			{
+				"nbsp" => Environment.NewLine,
+				"lt" => "<",
+				"gt" => ">",
+				"amp" => "&",
+				"quot" => "\"",
+				_ => match.Value
+			};
+		});
+	}
+}
